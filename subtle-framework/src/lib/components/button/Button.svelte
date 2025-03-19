0f4d262e0e1c7d34b3b9315f3625eed527c9cdc1
Yash Gugale <yashgugale@gmail.com>
2025-03-19 00:38:48 -0400

    Adding svelte integration

---
+++ subtle-framework/src/lib/components/button/Button.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import './button.css';
+	export let text = 'Click me!';
+</script>
+
+<button class="btn">{text}</button>
---
+++ subtle-framework/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Button from '$lib/components/button/Button.svelte';
+</script>
+
+<Button text="Click me!" />
